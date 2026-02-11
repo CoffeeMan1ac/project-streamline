be087f809cca9de14043f91eee0c8f256c6a0a8d
orsonosullivan <orosulli@tcd.ie>
2026-02-11 19:24:42 +0000

    adds boilerplate controller

---
+++ backend/src/main/java/com/munichre/streamline/controller/ProductController.java
@@ -0,0 +1,11 @@
+package com.munichre.streamline.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+}
