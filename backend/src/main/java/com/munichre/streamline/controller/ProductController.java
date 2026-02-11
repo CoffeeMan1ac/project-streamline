8391bf15bd9982ebe33b3a1fd7ef73b4ca48e0a7
orsonosullivan <orosulli@tcd.ie>
2026-02-11 22:42:05 +0000

    Add ProductController

--- backend/src/main/java/com/munichre/streamline/controller/ProductController.java
+++ backend/src/main/java/com/munichre/streamline/controller/ProductController.java
@@ -1,5 +1,4 @@
 package com.munichre.streamline.controller;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,4 +7,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
+
+    private final ProductService productService;
+
+    //constructor injection
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> get_products() {
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
+    }
 }
