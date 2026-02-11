3b5fe3aca0157514fa9acefc39d00601a612a38f
orsonosullivan <orosulli@tcd.ie>
2026-02-11 22:55:55 +0000

    adding test file and imports

--- backend/src/test/java/com/munichre/streamline/controller/ProductControllerTest.java
+++ backend/src/test/java/com/munichre/streamline/controller/ProductControllerTest.java
@@ -1,9 +1,23 @@
 package com.munichre.streamline.controller;
 
+package com.munichre.streamline.controller;
+
+import com.munichre.streamline.model.Product;
+import com.munichre.streamline.repository.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ControllerTests {
 
 	@Test
