58a6a4ebfe650a33777c3c5bcc3b6bf1e2fb5509
Kevin Murphy <murphk35@tcd.ie>
2026-02-12 16:04:27 +0000

    create findActiveProducts query in repository

--- backend/src/main/java/com/munichre/streamline/repository/ProductRepository.java
+++ backend/src/main/java/com/munichre/streamline/repository/ProductRepository.java
@@ -2,8 +2,19 @@ package com.munichre.streamline.repository;
 
 import com.munichre.streamline.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+    @Query("""
+    SELECT p FROM Product p
+    WHERE p.active = true
+      AND p.startDate <= :now
+      AND (p.endDate IS NULL OR p.endDate >= :now)
+    """)
+    List<Product> findActiveProducts(@Param("now") LocalDateTime now);
 }
