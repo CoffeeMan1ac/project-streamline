e35c7c24f744f96480a6fb82b629e13f13bae770
Theresa James <thjames@tcd.ie>
2026-02-12 22:15:35 +0000

    Add navbar and footer across the app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import QuotesPage from "./pages/QuotesPage";
 
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/quotes" element={<QuotesPage />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,15 +1,11 @@
-import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
 import PricingSection from "../components/PricingSection";
-import Footer from "../components/Footer";
 
 const Home = () => {
   return (
           <>
-          <Navbar />
             <HeroSection />
             <PricingSection />
-            <Footer />
           </>
   );
 };
