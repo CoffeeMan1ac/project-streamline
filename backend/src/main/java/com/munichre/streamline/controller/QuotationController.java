ebec4f3ae040cb0ad746feb431567ca7085eee62
Orson O Sullivan <orosulli@tcd.ie>
2026-02-12 15:23:06 +0000

    adding quoterequest and response dto

--- backend/src/main/java/com/munichre/streamline/controller/QuotationController.java
+++ backend/src/main/java/com/munichre/streamline/controller/QuotationController.java
@@ -1,8 +1,12 @@
 package com.munichre.streamline.controller;
 
+import com.munichre.streamline.dto.QuoteRequest;
+import com.munichre.streamline.dto.QuoteResponse;
+import com.munichre.streamline.service.QuoteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/api/quotes")
