020a542abe1ef11518844bf48b85cfb9014a75de
orsonosullivan <orosulli@tcd.ie>
2026-02-12 20:22:27 +0000

    adding status code

--- backend/src/test/java/com/munichre/streamline/controller/ProductControllerTest.java
+++ backend/src/test/java/com/munichre/streamline/controller/ProductControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
@@ -19,6 +20,16 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(ProductController.class)
 class ProductControllerTest { 
-	
+	@Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private ProductService productService;
+
+    @Test
+    void getAllProducts_returnsOk() throws Exception {
+        mockMvc.perform(get("/api/products"))
+                .andExpect(status().isOk());
+    }
 }
 
