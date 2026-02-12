614e9023871f047a0dfc6d272f798cf4680c02c5
Orson O Sullivan <orosulli@tcd.ie>
2026-02-12 15:21:57 +0000

    Adds boilerplate controller

---
+++ backend/src/main/java/com/munichre/streamline/controller/QuotationController.java
@@ -0,0 +1,11 @@
+package com.munichre.streamline.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/quotes")
+@RequiredArgsConstructor
+public class QuotationController {
+}
