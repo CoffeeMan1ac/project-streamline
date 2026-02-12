76b2932bf33b68eda6e4ccf9b14759e44d48440c
Kevin Murphy <murphk35@tcd.ie>
2026-02-12 16:34:35 +0000

    test that activeProducts only returns what we consider active products

--- backend/src/test/java/com/munichre/streamline/model/ProductRuleIntegrationTest.java
+++ backend/src/test/java/com/munichre/streamline/model/ProductRuleIntegrationTest.java
@@ -1,10 +1,12 @@
 package com.munichre.streamline.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertLinesMatch;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -78,6 +80,123 @@ public class ProductRuleIntegrationTest {
 
         assertEquals(foundRules.get(0).getName(), rule1.getName());
         assertEquals(foundRules.get(1).getName(), rule2.getName());
+    }
+
+    @Test
+    void testFindActiveProducts() {
+        LocalDateTime today = LocalDateTime.now();
+        LocalDateTime yesterday = today.minusDays(1);
+        LocalDateTime tomorrow = today.plusDays(1);
+
+        Integer i = 1;
+
+        ArrayList<String> passes = new ArrayList<>();
+
+        // PASS - After Start, No End, Active
+        Product p = new Product();
+        p.setName("p".concat((i++).toString()));
+        p.setBaseRate(BigDecimal.valueOf(100));
+        p.setMostPopular(true);
+        p.setStartDate(yesterday);
+        p.setActive(true);
+        productRepository.save(p);
+        passes.add(p.getName());
+        
+        // Fail - After Start, No End, [Inactive]
+        p = new Product();
+        p.setName("p".concat((i++).toString()));
+        p.setBaseRate(BigDecimal.valueOf(100));
+        p.setMostPopular(true);
+        p.setStartDate(yesterday);
+        p.setActive(false);
+        productRepository.save(p);
+        
+        // PASS - After Start, Before End, Active
+        p = new Product();
+        p.setName("p".concat((i++).toString()));
+        p.setBaseRate(BigDecimal.valueOf(100));
+        p.setMostPopular(true);
+        p.setStartDate(yesterday);
+        p.setEndDate(tomorrow);
+        p.setActive(true);
+        productRepository.save(p);
+        passes.add(p.getName());
+        
+        // Fail - After Start, Before End, [Inactive]
+        p = new Product();
+        p.setName("p".concat((i++).toString()));
+        p.setBaseRate(BigDecimal.valueOf(100));
+        p.setMostPopular(true);
+        p.setStartDate(yesterday);
+        p.setEndDate(tomorrow);
+        p.setActive(false);
+        productRepository.save(p);
+
+        // Fail - After Start, [After End], Active
+        p = new Product();
+        p.setName("p".concat((i++).toString()));
+        p.setBaseRate(BigDecimal.valueOf(100));
+        p.setMostPopular(true);
+        p.setStartDate(yesterday);
+        p.setEndDate(yesterday);
+        p.setActive(true);
+        productRepository.save(p);
+        
+        // Fail - After Start, [After End], [Inactive]
+        p = new Product();
+        p.setName("p".concat((i++).toString()));
+        p.setBaseRate(BigDecimal.valueOf(100));
+        p.setMostPopular(true);
+        p.setStartDate(yesterday);
+        p.setEndDate(yesterday);
+        p.setActive(false);
+        productRepository.save(p);
+        
+        // Fail - [Before Start], No End, Active
+        p = new Product();
+        p.setName("p".concat((i++).toString()));
+        p.setBaseRate(BigDecimal.valueOf(100));
+        p.setMostPopular(true);
+        p.setStartDate(tomorrow);
+        p.setActive(true);
+        productRepository.save(p);
+        
+        // Fail - [Before Start], No End, [Inactive]
+        p = new Product();
+        p.setName("p".concat((i++).toString()));
+        p.setBaseRate(BigDecimal.valueOf(100));
+        p.setMostPopular(true);
+        p.setStartDate(tomorrow);
+        p.setActive(false);
+        productRepository.save(p);
+        
+        // Fail - [Before Start], Before End, Active
+        p = new Product();
+        p.setName("p".concat((i++).toString()));
+        p.setBaseRate(BigDecimal.valueOf(100));
+        p.setMostPopular(true);
+        p.setStartDate(tomorrow);
+        p.setEndDate(tomorrow);
+        p.setActive(true);
+        productRepository.save(p);
+        
+        // Fail - [Before Start], Before End, [Inactive]
+        p = new Product();
+        p.setName("p".concat((i++).toString()));
+        p.setBaseRate(BigDecimal.valueOf(100));
+        p.setMostPopular(true);
+        p.setStartDate(tomorrow);
+        p.setEndDate(tomorrow);
+        p.setActive(false);
+        productRepository.save(p);
+
+        productRepository.flush();
+
+        List<Product> activeProducts = productRepository.findActiveProducts(today);
+        List<String> activeNames = activeProducts.stream()
+            .map(product -> product.getName()).toList();
+
+        assertLinesMatch(passes, activeNames);
 
     }
 }
