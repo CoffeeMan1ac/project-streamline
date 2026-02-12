f7cf14b3ab3adcac7f43a6d3e1a3869e5f825a02
Catherine Neumeyer <neumeyec@tcd.ie>
2026-02-12 23:36:15 +0000

    Add accept page

---
+++ frontend/src/pages/AcceptPage.tsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import { Button, Container, Typography, Box } from "@mui/material";
+import checkmark from "../assets/checkmark.png";
+
+const AcceptPage = () => {
+    const [purchaseIsLoading, setPurchaseIsLoading] = useState(false);
+    const [backIsLoading, setBackIsLoading] = useState(false);
+
+    const handleProceed = async () => {
+        setPurchaseIsLoading(true);
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        setPurchaseIsLoading(false);
+    };
+
+    const handleBack = async () => {
+        setBackIsLoading(true);
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        setBackIsLoading(false);
+    };
+
+    return (
+        <>
+            <div style={{ backgroundColor: "#f7fbfc", minHeight: "100vh", padding: "20px" }}>
+                <Container maxWidth="sm" style={{ marginTop: "30px", maxWidth: "700px" }}>
+                    <Box 
+                        sx={{ 
+                            boxShadow: 3,
+                            p: 4, 
+                            bgcolor: "white", 
+                            borderRadius: 2, 
+                            textAlign: "center" 
+                        }}
+                    >
+                        <img src={checkmark} alt="Checkmark" style={{ width: 140, height: 120}} />
+                        
+                        <Typography variant="h4" gutterBottom style={{ color: "black", fontWeight: "bold" }}>
+                            Accepted!
+                        </Typography>
+                        <Typography variant="body1" style={{ color: "#4a4a4a"}}>
+                            Your quote for the <strong>Apple iPhone 15 Pro</strong> has been approved with a
+                        </Typography>
+                        <Typography variant="body1" style={{ color: "#4a4a4a", marginBottom: 27 }}>
+                            premium of <span style={{ fontSize: "1.5rem", color: "#0167b2", fontWeight: "bold" }}>€10.00</span> per month.
+                        </Typography>
+                        
+                        <Box sx={{ display: "flex", gap: 2, justifyContent: "center" }}>
+                            <Button
+                                variant="contained"
+                                disableElevation
+                                disabled={purchaseIsLoading}
+                                onClick={handleProceed}
+                                style={{ 
+                                    padding: "12px 24px", 
+                                    backgroundColor: "#0167b2",
+                                    color: "white",
+                                    textTransform: "none",
+                                    fontWeight: "bold",
+                                    width: "200px", 
+                                    height: "45px"
+                                }}
+                            >
+                                {purchaseIsLoading ? "Proceeding..." : "Proceed to Purchase"}
+                            </Button>
+                            <Button
+                                variant="contained"
+                                disableElevation
+                                disabled={backIsLoading}
+                                onClick={handleBack}
+                                style={{ 
+                                    padding: "12px 24px", 
+                                    backgroundColor: "#e6e7eb",
+                                    color: "#3b444b",
+                                    textTransform: "none",
+                                    fontWeight: "bold",
+                                    width: "160px",
+                                    height: "45px"
+                                }}
+                            >
+                                {backIsLoading ? "Returning..." : "Back to Home"}
+                            </Button>
+                        </Box>
+                    </Box>
+                </Container>
+            </div>
+        </>
+    );
+}
+
+export default AcceptPage;
