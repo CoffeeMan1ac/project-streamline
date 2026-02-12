224bf65be4ae32a8792c7162c118030436d6d366
Denys Keleshohlu <keleshod@tcd.ie>
2026-02-12 14:01:35 +0000

    Add basic rule decision engine service

--- backend/src/main/java/com/munichre/streamline/service/RuleEvaluationService.java
+++
@@ -1,8 +0,0 @@
-package com.munichre.streamline.service;
-
-import com.munichre.streamline.dto.EvaluationResult;
-import java.util.Map;
-
-public interface RuleEvaluationService {
-    EvaluationResult evaluate(Map<String, Object> data);
-}
