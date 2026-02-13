401c45edd3bccd625bfe0c1b9369b3a28c5f7fff
Keith Horgan <khorgan@tcd.ie>
2026-02-13 18:14:08 +0000

    Add product dto

---
+++ backend/src/main/java/com/munichre/streamline/dto/ProductDto.java
@@ -0,0 +1,16 @@
+package com.munichre.streamline.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+public record ProductDto(
+        UUID id,
+        BigDecimal baseRate,
+        String name,
+        String description,
+        boolean mostPopular,
+        ProductTypeDto type,
+        List<CoverageDto> coverages,
+        List<CoverageDto> exclusions) {
+}
