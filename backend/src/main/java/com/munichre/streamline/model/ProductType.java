e9fe5ab1167601012b13ee9e47064546fc7ad9fa
Keith Horgan <khorgan@tcd.ie>
2026-02-13 16:28:14 +0000

    Add product type model

---
+++ backend/src/main/java/com/munichre/streamline/model/ProductType.java
@@ -0,0 +1,40 @@
+package com.munichre.streamline.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "product_types")
+@Data
+@NoArgsConstructor @AllArgsConstructor
+public class ProductType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String code;
+
+    @Column(nullable = false)
+    private String label;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
