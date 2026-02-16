d7f42f7201f3845b114f882f3bc5813f9ea8d456
orsonosullivan <orosulli@tcd.ie>
2026-02-16 16:38:51 +0000

    fixs test to pass pipeline

--- frontend/src/components/tests/Navbar.test.tsx
+++ frontend/src/components/tests/Navbar.test.tsx
@@ -1,18 +1,18 @@
 import "@testing-library/jest-dom/vitest";
 import { render, screen } from "@testing-library/react";
 import { describe, test, expect } from "vitest";
+import { MemoryRouter } from "react-router-dom";
 import Navbar from "../Navbar";
 
 describe("Navbar", () => {
   test("renders logo and title", () => {
-    render(<Navbar />);
+    render(
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>
+    );
+
     expect(screen.getByAltText(/Phone Shield logo/i)).toBeInTheDocument();
     expect(screen.getByText(/Phone Shield/i)).toBeInTheDocument();
   });
-
-  // test("renders navigation buttons", () => {
-  //   render(<Navbar />);
-  //   expect(screen.getByRole("button", { name: /Home/i })).toBeInTheDocument();
-  //   expect(screen.getByRole("button", { name: /Get a Quote/i })).toBeInTheDocument();
-  // });
 });
