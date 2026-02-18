75175004ad1fb4b26f00134dd4a9505c758a1187
Keith Horgan <khorgan@tcd.ie>
2026-02-18 16:51:43 +0000

    Fix navbar test to use memory router

--- frontend/src/components/tests/Navbar.test.tsx
+++ frontend/src/components/tests/Navbar.test.tsx
@@ -7,12 +7,14 @@ import Navbar from "../Navbar";
 describe("Navbar", () => {
   test("renders logo and title", () => {
     render(
-      <Navbar
-        mode={"light"}
-        toggleTheme={function (): void {
-          throw new Error("Function not implemented.");
-        }}
-      />
+      <MemoryRouter>
+        <Navbar
+          mode={"light"}
+          toggleTheme={function (): void {
+            throw new Error("Function not implemented.");
+          }}
+        />
+      </MemoryRouter>
     );
     expect(screen.getByAltText(/Phone Shield logo/i)).toBeInTheDocument();
     expect(screen.getByText(/Phone Shield/i)).toBeInTheDocument();
