b103a8f67f0b7c872ac2ffd82aaad63aaf69a109
Kevin Murphy <murphk35@tcd.ie>
2026-02-25 10:07:03 +0000

    create controller route for getRules

--- backend/src/main/java/com/munichre/streamline/controller/RuleController.java
+++ backend/src/main/java/com/munichre/streamline/controller/RuleController.java
@@ -1,6 +1,8 @@
 package com.munichre.streamline.controller;
 
+import com.munichre.streamline.model.Rule;
 import com.munichre.streamline.service.RuleService;
+import java.util.List;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,4 +21,11 @@ public class RuleController {
     ruleService.createRule(payload);
     return ResponseEntity.status(HttpStatus.CREATED).build();
   }
+
+  @GetMapping
+    public ResponseEntity<List<Rule>> getRules(@RequestBody Map<String, Object> payload) {
+    List<Rule> rules = ruleService.getRules(payload);
+    return ResponseEntity.ok(rules);
+  }
+
 }
