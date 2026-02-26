59621e7d7a4321612a5a85132b09a8cc7085a102
Daniel Byrd <dbyrd@tcd.ie>
2026-02-26 15:31:23 +0000

    Add text and button to Rules page

--- frontend/src/pages/RulesManagementPage.tsx
+++ frontend/src/pages/RulesManagementPage.tsx
@@ -1,6 +1,9 @@
+import Button from "@mui/material/Button";
 import RuleTable from "../components/RuleTable";
 import SelectProduct from "../components/SelectProduct";
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
 const testRules: {
   order: number;
   ruleName: string;
@@ -39,6 +42,21 @@ const RulesManagementPage = () => {
   return (
     <>
       <Box sx={{ mx: 20, my: 4 }}>
+        <Box>
+          <Box>
+            <Typography variant="h4" sx={{ fontWeight: 700 }}>
+              Rules Management
+            </Typography>
+            <Typography variant="body1" color="text.secondary" sx={{ mt: 1 }}>
+              Manage underwriting rules per product
+            </Typography>
+          </Box>
+          <Box>
+            <Button>
+              + Create New Rule
+            </Button>
+        </Box>
+        </Box>
         <Box sx={{ my: 4 }}>
           <SelectProduct />
         </Box>
