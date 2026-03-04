f2b1de7e907d53ed47490378adb7ee739a5d86be
Daniel Byrd <dbyrd@tcd.ie>
2026-03-04 16:52:39 +0000

    First draft of Auth Card

--- frontend/src/components/AuthCard.tsx
+++ frontend/src/components/AuthCard.tsx
@@ -1,11 +1,26 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, TextField, Button } from "@mui/material";
 
 const AuthCard = () => {
   return (
     <Box>
-      <Typography variant="h4" component="h1" gutterBottom>
-        Authentication
+      <Box>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Email / Username
+        </Typography>
+        <TextField id="outlined-basic" label="Email / Username" variant="outlined" />
+      </Box>
+      <Box>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Password
+        </Typography>
+        <TextField id="outlined-password" label="Password" variant="outlined" />
+      </Box>
+      <Typography variant="body2" color="text.secondary" sx={{ mt: 2 }}>
+        Forgot password?
       </Typography>
+      <Button variant="contained" color="primary" sx={{ mt: 2 }}>
+        Log In
+      </Button>
     </Box>
   );
 };
