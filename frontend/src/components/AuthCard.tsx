ae8de36c6bb673ca2050c5571541f807b255cfdd
Daniel Byrd <dbyrd@tcd.ie>
2026-03-04 16:33:12 +0000

    add files and skeleton

---
+++ frontend/src/components/AuthCard.tsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from "@mui/material";
+
+const AuthCard = () => {
+  return (
+    <Box>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Authentication
+      </Typography>
+    </Box>
+  );
+};
+
+export default AuthCard;
