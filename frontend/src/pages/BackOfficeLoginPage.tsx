854c87f38295209e45593c24aa2d326e41a32ae9
Daniel Byrd <dbyrd@tcd.ie>
2026-03-04 17:55:49 +0000

    Added logo in circle above login

--- frontend/src/pages/BackOfficeLoginPage.tsx
+++ frontend/src/pages/BackOfficeLoginPage.tsx
@@ -1,6 +1,7 @@
 import AuthCard from "../components/AuthCard";
 import Box from "@mui/material/Box";
-import { Typography } from "@mui/material";
+import { Typography, Button } from "@mui/material";
+
 const BackOfficeLoginPage = () => {
   return (
     <>
@@ -11,13 +12,38 @@ const BackOfficeLoginPage = () => {
         alignItems="center"
         minHeight="100vh"
       >
-        <Typography variant="h4" component="h1" style={{ fontWeight: "bold" }} gutterBottom>
-          Phone Shield
-        </Typography>
-        <Typography gutterBottom mb={5} sx={{ color: "text.secondary" }}>
-          Back Office Portal Login
-        </Typography>
-        <AuthCard />
+            <Box
+            sx={{
+                width: 50,
+                height: 50,
+                borderRadius: "50%",
+                backgroundColor: "primary.main",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                overflow: "hidden",
+            }}
+            >
+            <img
+                src="shield_logo.png"
+                alt="Logo"
+                style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "contain",
+                }}
+            />
+            </Box>
+            <Typography variant="h4" component="h1" style={{ fontWeight: "bold" }} gutterBottom>
+            Phone Shield
+            </Typography>
+            <Typography gutterBottom mb={5} sx={{ color: "text.secondary" }}>
+            Back Office Portal Login
+            </Typography>
+            <AuthCard />
+            <Button variant="text" size="small" sx={{ color: "text.secondary", mt: 3 }}>
+            Back to Home
+            </Button>
       </Box>
     </>
   );
