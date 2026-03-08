52ff3358c69647bf93e1ea64f8f57ac8f3fffbf3
Kevin Murphy <murphk35@tcd.ie>
2026-03-08 18:31:45 +0000

    Add repo method to return all products (active and inactive)

--- backend/src/main/java/com/munichre/streamline/product/repository/ProductRepository.java
+++ backend/src/main/java/com/munichre/streamline/product/repository/ProductRepository.java
@@ -37,6 +37,26 @@ public interface ProductRepository extends JpaRepository<Product, UUID> {
 
   @Query(
       """
+              SELECT new com.munichre.streamline.product.repository.dto.ProductRowDto(
+                p.id,
+                p.baseRate,
+                p.name,
+                p.description,
+                p.startDate,
+                p.endDate,
+                p.active,
+                t.id,
+                t.code,
+                t.label
+              )
+              FROM Product p
+              JOIN p.type t
+              ORDER BY p.baseRate asc
+            """)
+  List<ProductRowDto> findProductRows();
+
+  @Query(
+      """
               SELECT new com.munichre.streamline.product.api.dto.ProductOptionDto(
                 p.id,
                 p.name
