8f003a0c85611f7e2302783da302ba336e66649a
Keith Horgan <khorgan@tcd.ie>
2026-03-10 11:08:53 +0000

    Remove unused code

--- backend/src/main/java/com/munichre/streamline/service/ConditionEvaluator.java
+++
@@ -1,5 +0,0 @@
-package com.munichre.streamline.service;
-
-public interface ConditionEvaluator {
-  boolean evaluate(Object fieldValue, String operator, Object compareValue);
-}
--- backend/src/main/java/com/munichre/streamline/service/PremiumCalculator.java
+++
@@ -1,8 +0,0 @@
-package com.munichre.streamline.service;
-
-import java.math.BigDecimal;
-import java.util.Map;
-
-public interface PremiumCalculator {
-  BigDecimal calculate(Map<String, Object> data);
-}
