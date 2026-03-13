8c30ce588fd8e6a78d19668c6c31591ad5527f2b
orsonosullivan <orosulli@tcd.ie>
2026-03-13 13:33:53 +0000

    changes api route to rely on subdomain

--- backend/src/main/java/com/munichre/streamline/constant/ApiRoutes.java
+++ backend/src/main/java/com/munichre/streamline/constant/ApiRoutes.java
@@ -5,9 +5,9 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ApiRoutes {
-  public static final String CUSTOMER_API_ROOT = "/api/customer/**";
-  public static final String BACKOFFICE_API_ROOT = "/api/backoffice/**";
+  public static final String CUSTOMER_API_ROOT = "/customer/**";
+  public static final String BACKOFFICE_API_ROOT = "/backoffice/**";
 
-  public static final String CUSTOMER_API_BASE = "/api/customer";
-  public static final String BACKOFFICE_API_BASE = "/api/backoffice";
+  public static final String CUSTOMER_API_BASE = "/customer";
+  public static final String BACKOFFICE_API_BASE = "/backoffice";
 }
