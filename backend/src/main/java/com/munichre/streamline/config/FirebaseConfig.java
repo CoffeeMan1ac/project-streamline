bf660b3068603889830cb4a7fe1f094f9eb23118
Orson O Sullivan <orosulli@tcd.ie>
2026-03-13 09:41:46 +0000

    change firebase config to get credentials form google not file

--- backend/src/main/java/com/munichre/streamline/config/FirebaseConfig.java
+++ backend/src/main/java/com/munichre/streamline/config/FirebaseConfig.java
@@ -4,28 +4,19 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
-import java.io.FileInputStream;
 import java.io.IOException;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FirebaseConfig {
 
-  @Value("${google.application.credentials:#{null}}")
-  private String credentialsPath;
-
   @PostConstruct
   public void initialize() throws IOException {
-    if (credentialsPath != null) {
-      FileInputStream serviceAccount = new FileInputStream(credentialsPath);
-      FirebaseOptions options =
-          FirebaseOptions.builder()
-              .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-              .build();
-      FirebaseApp.initializeApp(options);
-    } else {
-      FirebaseApp.initializeApp();
-    }
+    FirebaseOptions options =
+        FirebaseOptions.builder()
+            .setCredentials(GoogleCredentials.getApplicationDefault())
+            .build();
+
+    FirebaseApp.initializeApp(options);
   }
 }
