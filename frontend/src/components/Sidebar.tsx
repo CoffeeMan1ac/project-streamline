8c3654f69b33496a411452e5e16bb0592855dfe4
Daniel Byrd <dbyrd@tcd.ie>
2026-03-13 01:35:12 +0000

    Sort out spacing for buttons and bottom of sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -4,7 +4,7 @@ import MenuIcon from "@mui/icons-material/Menu";
 const Sidebar = () => {
   return (
     <Drawer variant="permanent" anchor="left">
-      <Box p={2}>
+      <Box p={2} display="flex" flexDirection="column" height="100%">
         {/* header */}
         <Box pb={1} borderBottom={1} borderColor="divider">
           <Box display="flex" alignItems="center" gap={2}>
@@ -39,6 +39,14 @@ const Sidebar = () => {
           <Button>Rules Management</Button>
           <Button>Dashboard</Button>
           <Button>Dashboard</Button>
+        </Box>
+
+        {/* footer */}
+        <Box borderTop={1} borderColor="divider" mt="auto" pt={2}>
+          <Box display="flex" flexDirection="column" alignItems="center" gap={1}>
+            <Typography variant="h6">Admin User</Typography>
+            <Typography variant="body2">Underwriter</Typography>
+          </Box>
         </Box>
       </Box>
     </Drawer>
