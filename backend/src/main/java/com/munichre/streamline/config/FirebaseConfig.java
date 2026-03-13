d478dbb78ea484016ff4d6d270230bc6a9922a1e
orsonosullivan <orosulli@tcd.ie>
2026-03-13 17:49:47 +0000

    formatting

--- backend/src/main/java/com/munichre/streamline/config/FirebaseConfig.java
+++ backend/src/main/java/com/munichre/streamline/config/FirebaseConfig.java
@@ -39,10 +39,8 @@ public class FirebaseConfig {
       }
     }
 
-    FirebaseOptions options = FirebaseOptions.builder()
-        .setCredentials(credentials)
-        .setProjectId(projectId)
-        .build();
+    FirebaseOptions options =
+        FirebaseOptions.builder().setCredentials(credentials).setProjectId(projectId).build();
 
     FirebaseApp.initializeApp(options);
   }
