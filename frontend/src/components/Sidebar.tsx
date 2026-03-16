2e82c68f67410cd3f5e8280cff3ce8e3bfa00160
Theresa James <thjames@tcd.ie>
2026-03-16 16:57:35 +0000

    Fix Lint

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -3,7 +3,6 @@ import MenuIcon from "@mui/icons-material/Menu";
 import FeedOutlinedIcon from "@mui/icons-material/FeedOutlined";
 import ViewInArOutlinedIcon from "@mui/icons-material/ViewInArOutlined";
 import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
-import { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import { auth } from "../config/firebase";
