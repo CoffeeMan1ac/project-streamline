128ec3d6bc601d5b1186881894cae07705892f7f
Daniel Byrd <dbyrd@tcd.ie>
2026-03-19 17:00:11 +0000

    Add MUI icons to left side

--- frontend/src/components/QuotationDetails.tsx
+++ frontend/src/components/QuotationDetails.tsx
@@ -1,4 +1,6 @@
 import { Box, Typography, Chip, Paper } from "@mui/material";
+import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
+import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
 
 const QuotationDetails = () => {
   return (
@@ -14,13 +16,18 @@ const QuotationDetails = () => {
         }}
       >
         <Box>
-          <Typography fontWeight={600} fontSize={18}>
-            PS-2024-001234
-          </Typography>
-
-          <Typography variant="body2" color="text.secondary">
-            2024-03-15 14:30
-          </Typography>
+          <Box display="flex" alignItems="center" gap={1}>
+            <DescriptionOutlinedIcon color="primary" sx={{ fontSize: 25, mr: 0.5 }} />
+            <Typography fontWeight={600} fontSize={25}>
+                PS-2024-001234
+            </Typography>
+          </Box>
+          <Box display="flex" alignItems="center" gap={1} mt={0.5}>
+            <CalendarTodayOutlinedIcon fontSize="small" sx={{ color: "text.secondary" }} />
+            <Typography variant="body2" color="text.secondary">
+                2024-03-15 14:30
+            </Typography>
+          </Box>
         </Box>
 
         <Chip
