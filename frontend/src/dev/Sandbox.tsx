b01b1dbfc73901662cf9b2a83a8d147bc766cef7
Theresa James <thjames@tcd.ie>
2026-03-20 22:50:39 +0000

    revert sandbox to original

--- frontend/src/dev/Sandbox.tsx
+++ frontend/src/dev/Sandbox.tsx
@@ -1,16 +1,7 @@
-import QuotationDetails from "../components/QuotationDetails";
-import Box from "@mui/material/Box";
 const Sandbox = () => {
   return (
     <>
       <h1>Sandbox for viewing components</h1>
-      <Box width="80%">
-        <QuotationDetails
-          quotationId="PS-2024-001234"
-          timeStamp="2024-03-15 14:30"
-          status="ACCEPTED"
-        />
-      </Box>
     </>
   );
 };
