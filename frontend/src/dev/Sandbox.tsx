2c88613214dbb0ddae3afbfd89c35f6295495f8c
Daniel Byrd <dbyrd@tcd.ie>
2026-03-20 23:51:15 +0000

    clear sandbox

--- frontend/src/dev/Sandbox.tsx
+++ frontend/src/dev/Sandbox.tsx
@@ -1,19 +1,7 @@
-import QuotePersonalDetails from "../components/QuotePersonalDetails";
-import { Box } from "@mui/material";
-
 const Sandbox = () => {
   return (
     <>
-      <Box width="80%">
-        <h1>Sandbox for viewing components</h1>
-        <QuotePersonalDetails
-          fullName="Daniel Byrd"
-          email="john.smith@email.com"
-          phone="+353 87 123 4567"
-          dateOfBirth="15 May 1992"
-          address="123 Main Street, Dublin 2, Ireland"
-        />
-      </Box>
+      <h1>Sandbox for viewing components</h1>
     </>
   );
 };
