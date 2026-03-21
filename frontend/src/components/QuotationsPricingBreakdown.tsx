4fcbe426807248647f40b9b7957bf6f1f723b93f
Daniel Byrd <dbyrd@tcd.ie>
2026-03-21 17:08:57 +0000

    Add file and skeleton

---
+++ frontend/src/components/QuotationsPricingBreakdown.tsx
@@ -0,0 +1,12 @@
+import { Box, Paper } from "@mui/material";
+
+const QuotationsPricingBreakdown = () => {
+  return (
+    <Box>
+      <Paper>
+      </Paper>
+    </Box>
+  );
+};
+
+export default QuotationsPricingBreakdown;
