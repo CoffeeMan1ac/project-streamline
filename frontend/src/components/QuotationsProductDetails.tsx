e584376fde99becefd46c7ac6080aeb32aa1060e
Daniel Byrd <dbyrd@tcd.ie>
2026-03-21 00:15:10 +0000

    Add styling

--- frontend/src/components/QuotationsProductDetails.tsx
+++ frontend/src/components/QuotationsProductDetails.tsx
@@ -1,11 +1,47 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Paper } from "@mui/material";
 
 const QuotationsProductDetails = () => {
   return (
-    <Box>
-      <Typography>
-        Product
-      </Typography>
+    <Box width="40%">
+      <Paper
+        sx={{
+          p: 3,
+          borderRadius: 2,
+          border: "1px solid",
+          borderColor: "divider",
+        }}
+      >
+        <Box>
+          <Typography
+            sx={{
+              fontSize: 16,
+              fontWeight: 600,
+              lineHeight: 1,
+            }}
+          >
+                Product Details
+            </Typography>
+        </Box>
+          <Typography
+            sx={{
+              fontSize: 11,
+              color: "text.secondary",
+              fontWeight: 600,
+              letterSpacing: 0.5,
+              mb: 0.5,
+            }}
+          >
+            Product
+        </Typography>
+          <Typography
+            sx={{
+              fontSize: 14,
+              fontWeight: 500,
+            }}
+          >
+            Premium Shield
+        </Typography>
+      </Paper>
     </Box>
   );
 };
--- frontend/src/dev/Sandbox.tsx
+++ frontend/src/dev/Sandbox.tsx
@@ -1,7 +1,9 @@
+import QuotationsProductDetails from "../components/QuotationsProductDetails";
 const Sandbox = () => {
   return (
     <>
       <h1>Sandbox for viewing components</h1>
+      <QuotationsProductDetails />
     </>
   );
 };
