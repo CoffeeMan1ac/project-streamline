6eb60b2ba055606b1964f31ac0575ac581a1c398
Daniel Byrd <dbyrd@tcd.ie>
2026-03-21 00:00:00 +0000

    Add file and skeleton

---
+++ frontend/src/components/QuotationsProductDetails.tsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from "@mui/material";
+
+const QuotationsProductDetails = () => {
+  return (
+    <Box>
+      <Typography>
+        Product
+      </Typography>
+    </Box>
+  );
+};
+
+export default QuotationsProductDetails;
