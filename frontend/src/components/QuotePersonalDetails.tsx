c69309bd273c6db1cd279af98413e3c2d4694ade
Theresa James <thjames@tcd.ie>
2026-03-21 20:30:36 +0000

    Make personal details component responsive on smaller devices

--- frontend/src/components/QuotePersonalDetails.tsx
+++ frontend/src/components/QuotePersonalDetails.tsx
@@ -21,7 +21,7 @@ const QuotePersonalDetails = ({
   address,
 }: QuotePersonalDetailsProps) => {
   return (
-    <Box width="40%">
+    <Box width={{ xs: "100%", md: "40%" }}>
       <Paper
         sx={{
           p: 3,
