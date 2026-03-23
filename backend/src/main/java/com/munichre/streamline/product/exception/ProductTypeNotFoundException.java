ad5e023b12951568475342e0e292635003ea2830
Denys Keleshohlu <keleshod@tcd.ie>
2026-03-23 12:38:17 +0000

    Fix ProductTypeNotFoundException to extend BaseApplicationException with NOT_FOUND

--- backend/src/main/java/com/munichre/streamline/product/exception/ProductTypeNotFoundException.java
+++ backend/src/main/java/com/munichre/streamline/product/exception/ProductTypeNotFoundException.java
@@ -1,7 +1,10 @@
 package com.munichre.streamline.product.exception;
 
-public class ProductTypeNotFoundException extends RuntimeException {
+import com.munichre.streamline.exception.BaseApplicationException;
+import org.springframework.http.HttpStatus;
+
+public class ProductTypeNotFoundException extends BaseApplicationException {
   public ProductTypeNotFoundException() {
-    super("Invalid product type supplied.");
+    super("Invalid product type supplied.", HttpStatus.NOT_FOUND);
   }
 }
