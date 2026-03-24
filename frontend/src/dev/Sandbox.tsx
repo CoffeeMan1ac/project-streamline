b6f22f545ed7f49cf8330ea547e4240e5bcf668b
Daniel Byrd <dbyrd@tcd.ie>
2026-03-24 11:25:15 +0000

    Clear Sandbox

--- frontend/src/dev/Sandbox.tsx
+++ frontend/src/dev/Sandbox.tsx
@@ -1,43 +1,8 @@
-import { useState } from "react";
-import EditTag from "../components/EditTag";
-import { Button, Box, Typography } from "@mui/material";
-
-const initialValues = {
-  name: "Priority",
-  key: "priority",
-  color: "blue",
-};
-
 const Sandbox = () => {
-  const [open, setOpen] = useState(true);
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const handleUpdate = (data: { name: string; key: string; color: string }) => {
-    console.log("Updated tag:", data);
-    setOpen(false);
-  };
-
   return (
-    <Box sx={{ p: 4 }}>
-      <Typography variant="h4" sx={{ mb: 3 }}>
-        Sandbox for viewing components
-      </Typography>
-
-      <Button variant="contained" onClick={() => setOpen(true)}>
-        Open EditTag
-      </Button>
-
-      <EditTag
-        key={initialValues.key}
-        open={open}
-        onClose={handleClose}
-        onUpdate={handleUpdate}
-        initialValues={initialValues}
-      />
-    </Box>
+    <>
+      <h1>Sandbox for viewing components</h1>
+    </>
   );
 };
 
