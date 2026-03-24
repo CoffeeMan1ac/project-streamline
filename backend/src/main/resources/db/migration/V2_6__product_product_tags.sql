79aa7838eda9f4a59b2fdff6a9d0d56228f78904
Orson O Sullivan <orosulli@tcd.ie>
2026-03-24 23:26:05 +0000

    Edit V2_6__product_product_tags.sql

--- backend/src/main/resources/db/migration/V2_6__product_product_tags.sql
+++ backend/src/main/resources/db/migration/V2_6__product_product_tags.sql
@@ -1,21 +1,19 @@
 INSERT INTO
-    product_tags (id, code, label, created_at, updated_at)
+    product_product_tags (product_id, product_tag_id)
 VALUES
     (
-        '81a8e5b1-2356-434b-bf1c-78d2be5c9090',
-        'GREEN',
-        'Eco Friendly',
-        now (),
-        now ()
+        'bd7e7bd6-201f-466a-9155-74757a2b4b95', -- Standard Shield Green
+        '81a8e5b1-2356-434b-bf1c-78d2be5c9090' -- GREEN product tag
     ),
     (
-        '1ebe6012-74ac-43ea-9491-54ddff30ab6c',
-        'POPULAR',
-        'Most Popular',
-        now (),
-        now ()
-    ) ON CONFLICT (code) DO
-UPDATE
-SET
-    label = EXCLUDED.label,
-    updated_at = now ();
+        'e4c364c6-962f-48bc-9eea-c36ac5091dc9', -- Premium Shield Green
+        '81a8e5b1-2356-434b-bf1c-78d2be5c9090' -- GREEN product tag
+    ),
+    (
+        '4ca1c734-42b2-46b9-8710-b40800f89157', -- Global Shield Green
+        '81a8e5b1-2356-434b-bf1c-78d2be5c9090' -- GREEN product tag
+    ),
+    (
+        '5f257c25-924a-440a-87c0-e3d8efa635a4', -- Premium Shield
+        '1ebe6012-74ac-43ea-9491-54ddff30ab6c' -- POPULAR product tag
+    ) ON CONFLICT DO NOTHING;
