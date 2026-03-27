b3eda4400746830e4d6f14a1da87fd4692bc5b08
hantcd <hamckenn@tcd.ie>
2026-03-27 20:27:32 +0000

    Apply Spotless formatting

--- backend/src/main/java/com/munichre/streamline/config/FirebaseConfig.java
+++ backend/src/main/java/com/munichre/streamline/config/FirebaseConfig.java
@@ -1,18 +1,15 @@
 package com.munichre.streamline.config;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-
 import jakarta.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Profile("!test")
