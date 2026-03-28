edaeaca41b5622ee0c4070ec289de3fc8e2a4898
Theresa James <thjames@tcd.ie>
2026-03-28 23:01:00 +0000

    Edit db migration file

--- backend/src/main/resources/db/migration/R__10_fields.sql
+++ backend/src/main/resources/db/migration/R__10_fields.sql
@@ -2,16 +2,16 @@
 
 INSERT INTO fields (id, code, type, label, required, regex_pattern, options)
 VALUES
-  ('a1000000-0000-0000-0000-000000000001', 'first_name', 'text', 'First Name', true, '^[A-Za-z\s\-]{1,50}$', NULL),
-  ('a1000000-0000-0000-0000-000000000002', 'last_name', 'text', 'Last Name', true, '^[A-Za-z\s\-]{1,50}$', NULL),
-  ('a1000000-0000-0000-0000-000000000003', 'email', 'email', 'Email Address', true, '^[^@\s]+@[^@\s]+\.[^@\s]+$', NULL),
-  ('a1000000-0000-0000-0000-000000000004', 'phone_number', 'text', 'Phone Number', true, '^\+?[0-9\s\-]{7,15}$', NULL),
-  ('a1000000-0000-0000-0000-000000000005', 'date_of_birth', 'date', 'Date of Birth', true, NULL, NULL),
-  ('a1000000-0000-0000-0000-000000000006', 'device_make', 'text', 'Device Make', true, NULL, NULL),
-  ('a1000000-0000-0000-0000-000000000007', 'device_model', 'text', 'Device Model', true, NULL, NULL),
-  ('a1000000-0000-0000-0000-000000000008', 'device_value', 'number', 'Device Value', true, '^\d+(\.\d{1,2})?$', NULL),
-  ('a1000000-0000-0000-0000-000000000009', 'purchase_date', 'date', 'Purchase Date', true, NULL, NULL),
-  ('a1000000-0000-0000-0000-000000000010', 'device_condition', 'select', 'Device Condition', true, NULL, '["New", "Like New", "Good", "Fair"]')
+  ('a1000000-0000-0000-0000-000000000001', 'firstName', 'text', 'First Name', true, '^[A-Za-z\s\-]{1,50}$', NULL),
+  ('a1000000-0000-0000-0000-000000000002', 'lastName', 'text', 'Last Name', true, '^[A-Za-z\s\-]{1,50}$', NULL),
+  ('a1000000-0000-0000-0000-000000000003', 'emailAddress', 'email', 'Email Address', true, '^[^@\s]+@[^@\s]+\.[^@\s]+$', NULL),
+  ('a1000000-0000-0000-0000-000000000004', 'phoneNumber', 'text', 'Phone Number', true, '^\+?[0-9\s\-]{7,15}$', NULL),
+  ('a1000000-0000-0000-0000-000000000005', 'dateOfBirth', 'date', 'Date of Birth', true, NULL, NULL),
+  ('a1000000-0000-0000-0000-000000000006', 'phoneMake', 'text', 'Phone Make', true, NULL, NULL),
+  ('a1000000-0000-0000-0000-000000000007', 'phoneModel', 'text', 'Phone Model', true, NULL, NULL),
+  ('a1000000-0000-0000-0000-000000000008', 'country', 'text', 'Country', true, NULL, NULL),
+  ('a1000000-0000-0000-0000-000000000009', 'phoneAge', 'select', 'Phone Age', true, NULL, '["Less than 1 year", "1 year", "2 years", "3 years", "4+ years"]'),
+  ('a1000000-0000-0000-0000-000000000010', 'phoneCondition', 'select', 'Phone Condition', true, NULL, '["Brand New", "Lightly Used", "Good", "Heavily Used", "Damaged"]')
 ON CONFLICT (id) DO UPDATE SET
   code          = EXCLUDED.code,
   type          = EXCLUDED.type,
