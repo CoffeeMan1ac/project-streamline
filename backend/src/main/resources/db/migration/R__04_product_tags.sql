a2ff32123c0a6659acd123a24b7bc2ff7c39efa6
Theresa James <thjames@tcd.ie>
2026-03-30 17:42:11 +0100

    Fix colours

--- backend/src/main/resources/db/migration/R__04_product_tags.sql
+++ backend/src/main/resources/db/migration/R__04_product_tags.sql
@@ -1,12 +1,13 @@
 -- ${flyway:timestamp}
 
 INSERT INTO
-    product_tags (id, code, label, created_at, updated_at)
+    product_tags (id, code, label, color, created_at, updated_at)
 VALUES
     (
         '81a8e5b1-2356-434b-bf1c-78d2be5c9090',
         'GREEN',
         'Eco Friendly',
+        'green',
         now (),
         now ()
     ),
@@ -14,6 +15,7 @@ VALUES
         '1ebe6012-74ac-43ea-9491-54ddff30ab6c',
         'POPULAR',
         'Most Popular',
+        'blue',
         now (),
         now ()
     ) ON CONFLICT (code) DO
